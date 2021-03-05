5cd50881e1057770b64f65964c76d80d8da24027
Moisés Calzado <moisescalzado1227@gmail.com>
2021-03-05 14:25:44 +0100

    Añado el proyecto para el backend

---
+++ backend/src/test/java/com/gotacar/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gotacar.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
