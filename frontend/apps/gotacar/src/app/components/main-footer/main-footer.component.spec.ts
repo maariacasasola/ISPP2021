94ee2e6cdced5abe1aabe08423c61a034cbb9933
Moisés Calzado <moisescalzado1227@gmail.com>
2021-03-16 21:07:14 +0100

    fix tests

--- frontend/apps/gotacar/src/app/components/main-footer/main-footer.component.spec.ts
+++ frontend/apps/gotacar/src/app/components/main-footer/main-footer.component.spec.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MainFooterComponent } from './main-footer.component';
@@ -8,9 +9,9 @@ describe('MainFooterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MainFooterComponent ]
-    })
-    .compileComponents();
+      declarations: [MainFooterComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
   });
 
   beforeEach(() => {
--- frontend/apps/gotacar/src/app/pages/authenticated-page/authenticated-page.component.spec.ts
+++ frontend/apps/gotacar/src/app/pages/authenticated-page/authenticated-page.component.spec.ts
@@ -1,3 +1,4 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AuthenticatedPageComponent } from './authenticated-page.component';
@@ -8,9 +9,9 @@ describe('AuthenticatedPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AuthenticatedPageComponent ]
-    })
-    .compileComponents();
+      declarations: [AuthenticatedPageComponent],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
   });
 
   beforeEach(() => {
