6850b046bf86e1f1e7178dde2ff496885887b129
JoseG7TFG <jose.delgado.serrano@g7innovation.com>
2021-03-17 19:38:22 +0100

    (Implementacion) Modelado de User hecho
    Se ha realizado el modela de User y se ha creado un usuario por defecto

--- backend/src/main/java/com/gotacar/backend/BackendApplication.java
+++ backend/src/main/java/com/gotacar/backend/BackendApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.gotacar.backend.models.User;
 import com.gotacar.backend.models.UserRepository;
 
@@ -21,31 +25,18 @@ public class BackendApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
+	cargaData();
+
+
+	}
+	
+	private void cargaData() {
 		userRepository.deleteAll();
-
-		// save a couple of customers
-		userRepository.save(new User("Alice", "Smith"));
-		userRepository.save(new User("Bob", "Smith"));
-
-		// fetch all customers
-		System.out.println("Customers found with findAll():");
-		System.out.println("-------------------------------");
-		for (User customer : userRepository.findAll()) {
-			System.out.println(customer);
-		}
-		System.out.println();
-
-		// fetch an individual customer
-		System.out.println("Customer found with findByFirstName('Alice'):");
-		System.out.println("--------------------------------");
-		System.out.println(userRepository.findByFirstName("Alice"));
-
-		System.out.println("Customers found with findByLastName('Smith'):");
-		System.out.println("--------------------------------");
-		for (User customer : userRepository.findByLastName("Smith")) {
-			System.out.println(customer);
-		}
-
+		List<String> lista1 = new ArrayList<String>();
+		lista1.add("admin");	
+		LocalDate fecha1 =  LocalDate.of(2018, 10, 30);	
+		User usuario = new User("Manuel","Fernandez","2","manan@gmail.com","312312312","http://dasdasdas.com",fecha1,lista1);
+		userRepository.save(usuario);
 	}
 
 }
--- backend/src/main/java/com/gotacar/backend/models/User.java
+++ backend/src/main/java/com/gotacar/backend/models/User.java
@@ -1,24 +1,55 @@
 package com.gotacar.backend.models;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.annotation.Id;
 
+import lombok.Getter;
+import lombok.Setter;
+
+
+
+@Getter
+@Setter
 public class User {
   @Id
   public String id;
 
   public String firstName;
+
   public String lastName;
+
+  public String uid;
+
+  public String email;
+
+  public String dni;
+
+  public String profilePhoto;
+
+  public LocalDate birthdate;
+
+  public List<String> roles; 
+
 
   public User() {
   }
 
-  public User(String firstName, String lastName) {
+  public User(String firstName, String lastName, String uid, String email, String dni, String profilePhoto, LocalDate birthdate, List<String> roles) {
     this.firstName = firstName;
     this.lastName = lastName;
+    this.uid = uid;
+    this.email = email;
+    this.dni = dni;
+    this.profilePhoto = profilePhoto;
+    this.birthdate = birthdate;
+    this.roles = roles;
+
   }
 
   @Override
   public String toString() {
-    return String.format("Customer[id=%s, firstName='%s', lastName='%s']", id, firstName, lastName);
+    return String.format("Customer[id=%s]", id);
   }
 }
