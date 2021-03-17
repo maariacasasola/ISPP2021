c00b0c6802ab8f498b83f83db4265a78e6925ca9
Moisés Calzado <moisescalzado1227@gmail.com>
2021-03-17 18:12:52 +0100

    Añado endpoint de prueba para obtener los usuarios

--- backend/src/main/java/com/gotacar/backend/controllers/HelloController.java
+++ backend/src/main/java/com/gotacar/backend/controllers/HelloController.java
@@ -1,13 +1,22 @@
 package com.gotacar.backend.controllers;
 
+import java.util.List;
+
+import com.gotacar.backend.models.Trip;
+import com.gotacar.backend.models.TripRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    
-    @RequestMapping("/hello")
+
+	@Autowired
+	private TripRepository TripRepository;
+
+	@RequestMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Holaaaa", null);
 	}
@@ -15,5 +24,10 @@ public class HelloController {
 	@RequestMapping("/adios")
 	public String adios(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Adios", null);
+	}
+
+	@RequestMapping("/viajes")
+	public List<Trip> get_all_trips() {
+		return TripRepository.findAll();
 	}
 }
