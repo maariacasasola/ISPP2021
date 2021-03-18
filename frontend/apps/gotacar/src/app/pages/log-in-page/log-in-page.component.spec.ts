5b3074060fb9bbdfbcecb8a8a171302f34c56905
emicolalc <emicolalc@alum.us.es>
2021-03-18 20:14:51 +0100

    Implementación del login y logout

---
+++ frontend/apps/gotacar/src/app/pages/log-in-page/log-in-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LogInPageComponent } from './log-in-page.component';
+
+describe('LogInPageComponent', () => {
+  let component: LogInPageComponent;
+  let fixture: ComponentFixture<LogInPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LogInPageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LogInPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/apps/gotacar/src/app/pages/log-in-page/log-in-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthServiceService } from '../../services/auth-service.service';
+
+@Component({
+  selector: 'frontend-log-in-page',
+  templateUrl: './log-in-page.component.html',
+  styleUrls: ['./log-in-page.component.scss']
+})
+export class LogInPageComponent implements OnInit {
+
+  constructor(public authService: AuthServiceService) { }
+
+  ngOnInit(): void {
+  }
+
+}
