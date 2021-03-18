ca420c057700bfe98de37a43e86c1828afd27b4f
María <maariacasasola@gmail.com>
2021-03-18 18:50:04 +0100

    Añado dependencias de validaciones y creo ModelTests

--- backend/src/main/java/com/gotacar/backend/models/MeetingPoint.java
+++ backend/src/main/java/com/gotacar/backend/models/MeetingPoint.java
@@ -1,33 +1,47 @@
 package com.gotacar.backend.models;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
 import org.springframework.data.annotation.Id;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class MeetingPoint {
-  @Id
-  public String id;
+    @Id
+    public String id;
 
-  public Double lng;
-  public Double lat;
-  public String name;
-  public String address;
+    @NotNull
+    public Double lng;
+    @NotNull
+    public Double lat;
+    
+    public String name;
+    @NotBlank
+    @Length(max = 240, message = "La longitud de la dirección no puede ser mayor a 240")
+    public String address;
 
-  public MeetingPoint() {
-  }
+    public MeetingPoint() {
+    }
 
-  public MeetingPoint(Double lng, Double lat){
-      this.lng = lng;
-      this.lat = lat;
-  }
+    public MeetingPoint(Double lng, Double lat) {
+        this.lng = lng;
+        this.lat = lat;
+    }
 
-  public MeetingPoint(Double lng, Double lat, String address, String name){
-      this.lng = lng;
-      this.lat = lat;
-      this.address = address;
-      this.name = name;
-  }
+    public MeetingPoint(Double lng, Double lat, String address, String name) {
+        this.lng = lng;
+        this.lat = lat;
+        this.address = address;
+        this.name = name;
+    }
 
-  @Override
-  public String toString() {
-      return String.format("MeetingPoint[id=%s, name=%s]", id, name);
-  }
+    @Override
+    public String toString() {
+        return String.format("MeetingPoint[id=%s, name=%s]", id, name);
+    }
 }
---
+++ backend/src/test/java/com/gotacar/backend/MeetingPointModelTests.java
@@ -0,0 +1,43 @@
+package com.gotacar.backend;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+
+import com.gotacar.backend.models.MeetingPoint;
+
+import org.junit.jupiter.api.Test;
+
+import javax.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+public class MeetingPointModelTests {
+
+    private Validator createValidator() {
+        LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+        localValidatorFactoryBean.afterPropertiesSet();
+        return localValidatorFactoryBean;
+    }
+
+    private Validator validator = createValidator();
+
+    @Test
+    public void setMaxDirectionLength(){
+        MeetingPoint mp1 = new MeetingPoint(4.92384, 5.283749, "Un limón y medio limón, Dos limones y medio limón, Tres limones y medio limón, Cuatro limones y medio limón, Cinco limones y medio limón, Seis limones y medio limón, Siete limones y medio limón, Ocho limones y medio limón, Se que parece una película de Greenaway, Pero es tan solo un ejercicio de malabarismo, Me da lo mismo que nadie lo pueda entender, Yo y mis limones tenemos tanto de que hablar", "Nombre");
+        Set<ConstraintViolation<MeetingPoint>> constraintViolations = validator.validate(mp1);
+
+		ConstraintViolation<MeetingPoint> violation = constraintViolations.iterator().next();
+		assertThat(violation.getMessage()).isEqualTo("La longitud de la dirección no puede ser mayor a 240");
+    }
+
+    @Test
+    public void setNullLongitud() {
+        MeetingPoint mp1 = new MeetingPoint(null, 4.0310, "Name", "Direction");
+        Set<ConstraintViolation<MeetingPoint>> constraintViolations = validator.validate(mp1);
+
+        ConstraintViolation<MeetingPoint> violation = constraintViolations.iterator().next();
+        assertThat(violation.getMessage()).isEqualTo("no debe ser nulo");
+    }
+}
