6b6d9c8319b49b02f9d1ba5705f561534b30aaa6
María <maariacasasola@gmail.com>
2021-03-21 13:14:16 +0100

    Tests de listar viajes como administrador

--- backend/src/main/java/com/gotacar/backend/BackendApplication.java
+++ backend/src/main/java/com/gotacar/backend/BackendApplication.java
@@ -123,6 +123,7 @@ public class BackendApplication implements CommandLineRunner {
 			http.csrf().disable()
 					.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
 					.authorizeRequests().antMatchers(HttpMethod.POST, "/user").permitAll()
+					.antMatchers(HttpMethod.GET, "/list_trips").permitAll()
 					.antMatchers(HttpMethod.POST, "/search_trips").permitAll().anyRequest().authenticated();
 		}
 	}
