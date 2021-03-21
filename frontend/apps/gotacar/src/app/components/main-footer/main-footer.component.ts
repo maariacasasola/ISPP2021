51d29db3c28ddc73a25bbe6ae553ba4805ae27d1
micben1 <mike-sv@hotmail.com>
2021-03-21 21:06:29 +0100

    footer version1

--- frontend/apps/gotacar/src/app/components/main-footer/main-footer.component.ts
+++ frontend/apps/gotacar/src/app/components/main-footer/main-footer.component.ts
@@ -1,5 +1,7 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, Input, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'frontend-main-footer',
   templateUrl: './main-footer.component.html',
