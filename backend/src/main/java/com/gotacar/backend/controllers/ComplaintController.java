1dcdece0d877e8f94f9aaa11eeface29de8cf1de
María <maariacasasola@gmail.com>
2021-03-25 15:25:52 +0100

    Funcionalidad de backend de presentar queja

--- backend/src/main/java/com/gotacar/backend/controllers/ComplaintController.java
+++ backend/src/main/java/com/gotacar/backend/controllers/ComplaintController.java
@@ -1,10 +1,7 @@
 package com.gotacar.backend.controllers;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -19,11 +16,14 @@ import com.gotacar.backend.models.TripOrder.TripOrderRepository;
 
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class ComplaintController {
@@ -45,22 +45,38 @@ public class ComplaintController {
     @PostMapping("/complaints/create")
     @PreAuthorize("hasRole('ROLE_CLIENT')")
     public Complaint fileComplaint(@RequestBody String body) {
-        Complaint c = new Complaint();
+
         try {
             JsonNode jsonNode = objectMapper.readTree(body);
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            User user = userRepository.findByEmail(authentication.getPrincipal().toString());
 
             String tripId = objectMapper.readTree(jsonNode.get("tripId").toString()).asText();
-            ObjectId id = new ObjectId(tripId); 
-            
-            Trip t = tripRepository.findById(id);
+            ObjectId tripObjectId = new ObjectId(tripId);
+            Trip trip = tripRepository.findById(tripObjectId);
 
-            if (t.getEndingDate().isBefore(LocalDateTime.now())) {
+            List<TripOrder> lto = tripOrderRepository.userHasMadeTrip(new ObjectId(user.getId()), tripObjectId);
 
+            if (trip.getEndingDate().isBefore(LocalDateTime.now())) {
+                if (lto.size() == 1) {
+                    String content = objectMapper.readTree(jsonNode.get("content").toString()).asText();
+                    String title = objectMapper.readTree(jsonNode.get("title").toString()).asText();
+
+                    Complaint complaint = new Complaint(title, content, trip, user, LocalDateTime.now());
+
+                    this.complaintRepository.save(complaint);
+
+                    return complaint;
+                } else {
+                    throw new Exception("Usted no ha realizado este viaje");
+                }
+            } else {
+                throw new Exception("El viaje aún no se ha realizado");
             }
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
         }
-        return c;
+
     }
 
 }
--- backend/src/main/java/com/gotacar/backend/models/TripOrder/TripOrderRepository.java
+++ backend/src/main/java/com/gotacar/backend/models/TripOrder/TripOrderRepository.java
@@ -5,10 +5,10 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface TripOrderRepository extends MongoRepository<TripOrder, String> {
+public interface TripOrderRepository extends MongoRepository<TripOrder, String>, TripOrderRepositoryCustom  {
 
     public Optional<TripOrder> findById(String id);
 
     public List<TripOrder> findByUserUid(String uid);
-
+    
 }
