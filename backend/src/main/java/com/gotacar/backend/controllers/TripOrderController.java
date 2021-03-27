162849779728c5699e20523684dcc7fc7b19529d
GermanOctako <germanoctako@gmail.com>
2021-03-27 10:13:53 +0100

    Añado el test de controllador

--- backend/src/main/java/com/gotacar/backend/controllers/TripOrderController.java
+++ backend/src/main/java/com/gotacar/backend/controllers/TripOrderController.java
@@ -3,6 +3,8 @@ package com.gotacar.backend.controllers;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.gotacar.backend.models.User;
+import com.gotacar.backend.models.UserRepository;
 import com.gotacar.backend.models.TripOrder.TripOrder;
 import com.gotacar.backend.models.TripOrder.TripOrderRepository;
 
@@ -13,6 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
 
 @RestController
 @CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST })
@@ -21,16 +27,20 @@ public class TripOrderController {
     @Autowired
     private TripOrderRepository tripOrderRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @PreAuthorize("hasRole('ROLE_CLIENT')")
     @RequestMapping("/list_trip_orders")
-    public List<TripOrder> listTripOrders(@RequestParam("uid") String uid) {
-        List<TripOrder> lista = new ArrayList<>();
+    public List<TripOrder> listTripOrders() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = userRepository.findByEmail(authentication.getPrincipal().toString());
         try {
-            lista = tripOrderRepository.findByUserUid(uid);
+            return tripOrderRepository.findByUserUid(user.getUid());
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
+
         }
-        return lista;
     }
     
 }
