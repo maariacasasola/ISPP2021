dd11e804399271a3774f95e19a6f3b978c677b03
Moisés Calzado <moisescalzado1227@gmail.com>
2021-04-02 10:45:01 +0200

    Ver detalles de un viaje

--- frontend/apps/gotacar/src/app/components/trip-list/trip-list.component.ts
+++ frontend/apps/gotacar/src/app/components/trip-list/trip-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'frontend-trip-list',
@@ -8,7 +9,7 @@ import { Component, Input, OnInit } from '@angular/core';
 export class TripListComponent implements OnInit {
   @Input() trips;
 
-  constructor() {}
+  constructor(private _router: Router) {}
 
   ngOnInit(): void {}
 
@@ -16,5 +17,9 @@ export class TripListComponent implements OnInit {
     return trip?.driver?.profilePhoto
       ? trip?.driver?.profilePhoto
       : 'https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png';
+  }
+
+  see_trip_details(trip_id) {
+    this._router.navigate(['/', 'trip', trip_id]);
   }
 }
