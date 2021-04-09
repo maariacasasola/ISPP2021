2e858d622c7f6e970362e366e423b669580ad7f4
ChaoHu99 <chahu@alum.us.es>
2021-04-09 12:48:19 +0200

    Funcionalidad Listar usuarios para un conductor con sus tests

--- backend/src/main/java/com/gotacar/backend/controllers/UserController.java
+++ backend/src/main/java/com/gotacar/backend/controllers/UserController.java
@@ -1,21 +1,29 @@
 package com.gotacar.backend.controllers;
 
 import java.security.Key;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import com.gotacar.backend.models.User;
 import com.gotacar.backend.models.UserRepository;
+import com.gotacar.backend.models.Trip.Trip;
+import com.gotacar.backend.models.Trip.TripRepository;
+import com.gotacar.backend.models.TripOrder.TripOrder;
+import com.gotacar.backend.models.TripOrder.TripOrderRepository;
 import com.gotacar.backend.utils.TokenResponse;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +37,12 @@ public class UserController {
 
 	@Autowired
 	private UserRepository userRepository;
+
+	@Autowired
+	private TripRepository tripRepository;
+
+	@Autowired
+	private TripOrderRepository tripOrderRepository;
 
 	@PostMapping("user")
 	public TokenResponse login(@RequestParam("uid") String userId) {
@@ -59,4 +73,22 @@ public class UserController {
 				.setExpiration(new Date(System.currentTimeMillis() + 3600000)).signWith(key).compact();
 		return "Bearer " + token;
 	}
+
+	@PreAuthorize("hasRole('ROLE_DRIVER')")
+    @GetMapping("/list_users_trip/{tripId}")
+    public List<User> listUsersTrip(@PathVariable(value = "tripId") String tripId) {
+        try {
+            List<User> lista = new ArrayList<>();
+            Trip trip = tripRepository.findById(new ObjectId(tripId));
+			List<TripOrder> tripOrders = tripOrderRepository.findByTrip(trip);
+
+            for (TripOrder to: tripOrders){
+                lista.add(to.getUser());
+            }
+            return lista;
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage(), e);
+        }
+    }
+
 }
--- backend/src/test/java/com/gotacar/backend/controllers/UserControllerTest.java
+++ backend/src/test/java/com/gotacar/backend/controllers/UserControllerTest.java
@@ -7,20 +7,50 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.gotacar.backend.models.User;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.gotacar.backend.BackendApplication;
+import com.gotacar.backend.models.Location;
+import com.gotacar.backend.models.User;
+import com.gotacar.backend.models.UserRepository;
+import com.gotacar.backend.models.Trip.Trip;
+import com.gotacar.backend.models.Trip.TripRepository;
+import com.gotacar.backend.models.TripOrder.TripOrder;
+
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.security.test.context.support.WithMockUser;
+import com.gotacar.backend.controllers.UserControllerTest.TestConfig;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ContextConfiguration(classes = { TestConfig.class, BackendApplication.class })
 class UserControllerTest {
+
+	@Configuration
+	static class TestConfig {
+		@Bean
+		@Primary
+		public TripRepository mockB() {
+			TripRepository mockService = Mockito.mock(TripRepository.class);
+			return mockService;
+		}
+	}
 
 	@Autowired
 	private UserController controller;
@@ -28,16 +58,61 @@ class UserControllerTest {
 	@Autowired
 	private MockMvc mockMvc;
 
+	
+
+	private User user;
+	private User admin;
+	private User driver;
+	private Trip trip;
+	private TripOrder order;
+
+	@BeforeEach
+	void setUp() {
+		List<String> lista1 = new ArrayList<String>();
+		lista1.add("ROLE_ADMIN");
+		List<String> lista2 = new ArrayList<String>();
+		lista2.add("ROLE_CLIENT");
+		List<String> lista3 = new ArrayList<String>();
+		lista3.add("ROLE_CLIENT");
+		lista3.add("ROLE_DRIVER");
+
+		driver = new User("Jesús", "Márquez", "h9HmVQqlBQXD289O8t8q7aN2Gzg1", "driver@gotacar.es", "89070310K",
+				"http://dniclient.com", LocalDate.of(1999, 10, 10), lista3);
+		ObjectId driverObjectId = new ObjectId();
+		driver.setId(driverObjectId.toString());
+
+		user = new User("Martín", "Romero", "qG6h1Pc4DLbPTTTKmXdSxIMEUUE1", "client@gotacar.es", "89070336D",
+				"http://dniclient.com", LocalDate.of(1999, 10, 10), lista2);
+		ObjectId userObjectId = new ObjectId();
+		user.setId(userObjectId.toString());
+
+		admin = new User("Antonio", "Fernández", "Ej7NpmWydRWMIg28mIypzsI4BgM2", "admin@gotacar.es", "89070360G",
+				"http://dniadmin.com", LocalDate.of(1999, 10, 10), lista1);
+		ObjectId adminObjectId = new ObjectId();
+		admin.setId(adminObjectId.toString());
+
+		Location location1 = new Location("Cerro del Águila", "Calle Canal 48", 37.37536809507917, -5.96211306033204);
+		Location location2 = new Location("Viapol", "Av. Diego Martínez Barrio", 37.37625144174958, -5.976345387146261);
+		trip = new Trip(location1, location2, 220, LocalDateTime.of(2021, 05, 24, 16, 00, 00),
+				LocalDateTime.of(2021, 05, 24, 16, 15, 00), "Viaje desde Cerro del Águila hasta Triana", 3, driver);
+		ObjectId tripObjectId = new ObjectId();
+		trip.setId(tripObjectId.toString());
+
+		order = new TripOrder(trip, user, LocalDateTime.of(2021, 03, 20, 11, 45, 00), 350, "", 1);
+		ObjectId orderObjectId = new ObjectId();
+		order.setId(orderObjectId.toString());
+	}
+
 	@Test
 	public void testUserController() throws Exception {
-		mockMvc.perform(post("/user").param("uid", "Ej7NpmWydRWMIg28mIypzsI4BgM2")).andExpect(status().isOk());
+		mockMvc.perform(post("/user").param("uid", admin.getUid())).andExpect(status().isOk());
 		assertThat(controller).isNotNull();
 	}
 
 	@Test
 	@WithMockUser(value = "spring")
 	public void testCurrentUser() throws Exception {
-		String response = mockMvc.perform(post("/user").param("uid", "h9HmVQqlBQXD289O8t8q7aN2Gzg2"))
+		String response = mockMvc.perform(post("/user").param("uid", driver.getUid()))
         .andReturn().getResponse().getContentAsString();
 		org.json.JSONObject json2 = new org.json.JSONObject(response);
                 // Obtengo el token
@@ -49,5 +124,23 @@ class UserControllerTest {
         .accept(MediaType.APPLICATION_JSON));
 		assertThat(result.andReturn().getResponse().getStatus()).isEqualTo(200);
 		assertThat(result.andReturn().getResponse().getContentType().equals(User.class.toString()));
+	}
+	
+	@Test
+	@WithMockUser(value = "spring")
+	public void testFindUsersByTrip() throws Exception{
+		String response = mockMvc.perform(post("/user").param("uid", driver.getUid()))
+        .andReturn().getResponse().getContentAsString();
+		org.json.JSONObject json2 = new org.json.JSONObject(response);
+                // Obtengo el token
+        String token = json2.getString("token");
+		String tripId = trip.getId();
+
+		ResultActions result = mockMvc.perform(get("/list_users_trip/{tripId}", tripId).header("Authorization", token)
+        .contentType(MediaType.APPLICATION_JSON));
+
+		assertThat(result.andReturn().getResponse().getStatus()).isEqualTo(200);
+		assertThat(result.andReturn().getResponse().getErrorMessage()).isNull();
+
 	}
 }
