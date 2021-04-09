8c26bf9cf2cc671434e53ae93c244e01f3774f28
ferangmor <ferangmor@alum.us.es>
2021-04-09 17:54:06 +0200

    Listar usuarios Registrados y mockeo userControllerTests

--- backend/src/test/java/com/gotacar/backend/controllers/UserControllerTest.java
+++ backend/src/test/java/com/gotacar/backend/controllers/UserControllerTest.java
@@ -7,37 +7,134 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.gotacar.backend.models.User;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.bson.types.ObjectId;
+
+import com.gotacar.backend.BackendApplication;
+import com.gotacar.backend.controllers.TripControllerTest.TestConfig;
+import com.gotacar.backend.models.User;
+import com.gotacar.backend.models.UserRepository;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.security.test.context.support.WithMockUser;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ContextConfiguration(classes = { TestConfig.class, BackendApplication.class })
 class UserControllerTest {
-
+	
+	@Configuration
+	static class TestConfig {
+		@Bean
+		@Primary
+		public UserRepository mockB() {
+			UserRepository mockService = Mockito.mock(UserRepository.class);
+			return mockService;
+		}
+	}
 	@Autowired
 	private UserController controller;
 
+	@MockBean
+	private UserRepository userRepository;
+
 	@Autowired
 	private MockMvc mockMvc;
+	
+	private User user;
+	private User admin;
+	private User driver;
+	private User driver2;
+	
+	
+	@BeforeEach
+	void setUp() {
+		List<String> lista1 = new ArrayList<String>();
+		lista1.add("ROLE_ADMIN");
+		List<String> lista2 = new ArrayList<String>();
+		lista2.add("ROLE_CLIENT");
+		List<String> lista3 = new ArrayList<String>();
+		lista3.add("ROLE_CLIENT");
+		lista3.add("ROLE_DRIVER");
+
+		driver = new User("Jesús", "Márquez", "h9HmVQqlBQXD289O8t8q7aN2Gzg1", "driver@gotacar.es", "89070310K",
+				"http://dniclient.com", LocalDate.of(1999, 10, 10), lista3);
+		ObjectId driverObjectId = new ObjectId();
+		driver.setId(driverObjectId.toString());
+
+		driver2 = new User("Manuel", "Fernández", "h9HmVQqlBQXD289O8t8q7aN2Gzg2", "driver2@gmail.com", "312312312R",
+				"http://dniclient.com", LocalDate.of(1999, 10, 10), lista3);
+		ObjectId driverObjectId2 = new ObjectId();
+		driver2.setId(driverObjectId2.toString());
+
+		user = new User("Martín", "Romero", "qG6h1Pc4DLbPTTTKmXdSxIMEUUE1", "client@gotacar.es", "89070336D",
+				"http://dniclient.com", LocalDate.of(1999, 10, 10), lista2);
+		ObjectId userObjectId = new ObjectId();
+		user.setId(userObjectId.toString());
+		
+		admin = new User("Antonio", "Fernández", "Ej7NpmWydRWMIg28mIypzsI4BgM2", "admin@gotacar.es", "89070360G",
+				"http://dniadmin.com", LocalDate.of(1999, 10, 10), lista1);
+		ObjectId adminObjectId = new ObjectId();
+		admin.setId(adminObjectId.toString());
+		
+	}
 
 	@Test
 	public void testUserController() throws Exception {
-		mockMvc.perform(post("/user").param("uid", "Ej7NpmWydRWMIg28mIypzsI4BgM2")).andExpect(status().isOk());
+		Mockito.when(userRepository.findByUid(admin.getUid())).thenReturn(admin);
+		
+		mockMvc.perform(post("/user").param("uid", admin.getUid())).andExpect(status().isOk());
 		assertThat(controller).isNotNull();
 	}
 
 	@Test
+	public void testListEnrolledUsers() throws Exception{
+		List<User> lista = new ArrayList<User>();
+		lista.add(admin);
+		lista.add(driver);
+		lista.add(driver2);
+		lista.add(user);
+		Mockito.when(userRepository.findAll()).thenReturn(lista);
+		Mockito.when(userRepository.findByUid(admin.getUid())).thenReturn(admin);
+		
+		String response = mockMvc.perform(post("/user").param("uid", admin.getUid())).andReturn().getResponse()
+				.getContentAsString();
+
+		org.json.JSONObject json = new org.json.JSONObject(response);
+		String token = json.getString("token");
+		
+		ResultActions result = mockMvc
+				.perform(get("/list_enrolled_users").header("Authorization", token).contentType(MediaType.APPLICATION_JSON));
+		
+		assertThat(result.andReturn().getResponse().getStatus()).isEqualTo(200);
+		String res = result.andReturn().getResponse().getContentAsString();
+		assertThat(res.contains("ROLE_ADMIN")).isEqualTo(false);
+		
+	}
+	
+	@Test
 	@WithMockUser(value = "spring")
 	public void testCurrentUser() throws Exception {
-		String response = mockMvc.perform(post("/user").param("uid", "h9HmVQqlBQXD289O8t8q7aN2Gzg2"))
+		Mockito.when(userRepository.findByUid(admin.getUid())).thenReturn(admin);
+		Mockito.when(userRepository.findByEmail(admin.getEmail())).thenReturn(admin);
+		
+		String response = mockMvc.perform(post("/user").param("uid", admin.getUid()))
         .andReturn().getResponse().getContentAsString();
 		org.json.JSONObject json2 = new org.json.JSONObject(response);
                 // Obtengo el token
