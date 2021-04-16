f2fd6dcf4569425715fddee99d26dec701f7994e
emicolalc <emicolalc@alum.us.es>
2021-04-16 14:33:36 +0200

    Añado atributos a la vista de las solicitudes

--- frontend/apps/gotacar/src/app/pages/admin-page/admin-driver-requests-page/admin-driver-requests-page.component.ts
+++ frontend/apps/gotacar/src/app/pages/admin-page/admin-driver-requests-page/admin-driver-requests-page.component.ts
@@ -9,7 +9,7 @@ import { UsersService } from '../../../services/users.service';
 export class AdminDriverRequestsPageComponent {
   requests = [];
   constructor(private _users_service: UsersService, private _snackBar: MatSnackBar,
-    ) {
+  ) {
     this.load_driver_requests();
   }
 
@@ -24,20 +24,26 @@ export class AdminDriverRequestsPageComponent {
     }
   }
 
-  async accept_request(user){
-    try{
-      const message=await this._users_service.convert_to_driver(user?.uid);
-      if(message){
+  async accept_request(user) {
+    try {
+      const message = await this._users_service.convert_to_driver(user?.uid);
+      if (message) {
         await this.load_driver_requests();
         this._snackBar.open("Conductor creado correctamente", null, {
           duration: 3000,
         });
       }
-    }catch(error){
+    } catch (error) {
       console.log(error)
       this._snackBar.open("Ha ocurrido un error", null, {
         duration: 3000,
       });
     }
+  }
+
+  get_profile_photo(user) {
+    return user.profilePhoto
+      ? user.profilePhoto
+      : 'assets/img/default-user.jpg';
   }
 }
