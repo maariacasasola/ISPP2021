75d82d42f38df970efc877ff44edeedf885619c4
Moisés Calzado <moisescalzado1227@gmail.com>
2021-04-16 14:12:45 +0200

    Fix tests

--- frontend/apps/gotacar/src/app/pages/trip-search-result-page/trip-search-result-page.component.spec.ts
+++ frontend/apps/gotacar/src/app/pages/trip-search-result-page/trip-search-result-page.component.spec.ts
@@ -8,103 +8,96 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { SignUpComponent } from '../sign-up/sign-up.component';
 import { TripSearchResultPageComponent } from './trip-search-result-page.component';
 
-
 describe('TripSearchResultPageComponent', () => {
-    let component: TripSearchResultPageComponent;
-    let fixture: ComponentFixture<TripSearchResultPageComponent>;
+  let component: TripSearchResultPageComponent;
+  let fixture: ComponentFixture<TripSearchResultPageComponent>;
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            imports: [
-                ReactiveFormsModule,
-                RouterTestingModule,
-                MatSnackBarModule,
-                HttpClientTestingModule,
-                BrowserAnimationsModule,
-            ],
-            declarations: [SignUpComponent],
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule,
+        RouterTestingModule,
+        MatSnackBarModule,
+        HttpClientTestingModule,
+        BrowserAnimationsModule,
+      ],
+      declarations: [SignUpComponent],
 
-            schemas: [NO_ERRORS_SCHEMA],
-        }).compileComponents();
-    });
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TripSearchResultPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(TripSearchResultPageComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
+  it('should load search params', () => {
+    spyOn(component, 'load_search_params');
+    component.load_search_params();
+    fixture.detectChanges();
+    expect(component.load_search_params).toHaveBeenCalled();
+  });
 
-    it('should load search params', () => {
-        spyOn(component, 'load_search_params');
-        component.load_search_params();
-        fixture.detectChanges();
-        expect(component.load_search_params).toHaveBeenCalled();
-    });
+  it('should get search results', () => {
+    spyOn(component, 'get_search_results');
+    component.get_search_results();
+    fixture.detectChanges();
+    expect(component.get_search_results).toHaveBeenCalled();
+  });
 
-    it('should get search results', () => {
-        spyOn(component, 'get_search_results');
-        component.get_search_results();
-        fixture.detectChanges();
-        expect(component.get_search_results).toHaveBeenCalled();
-    });
+  it('should open snackbar', () => {
+    spyOn(component, 'openSnackBar');
+    component.openSnackBar('a,b,c');
+    fixture.detectChanges();
+    expect(component.openSnackBar).toHaveBeenCalled();
+  });
 
-    it('should open snackbar', () => {
-        spyOn(component, 'openSnackBar');
-        component.openSnackBar('a,b,c', 'Cerrar');
-        fixture.detectChanges();
-        expect(component.openSnackBar).toHaveBeenCalled();
-    });
+  it('should get right coordinates', () => {
+    spyOn(component, 'get_coordinates');
+    component.get_coordinates('Nervion');
+    fixture.detectChanges();
+    expect(component.get_coordinates).toHaveBeenCalled();
+  });
 
-    it('should get right coordinates', () => {
-        spyOn(component, 'get_coordinates');
-        component.get_coordinates('Nervion');
-        fixture.detectChanges();
-        expect(component.get_coordinates).toHaveBeenCalled();
-    });
+  it('should filter by limit date', () => {
+    component.tipIsInHour('2021-04-15T20:00:00');
+    fixture.detectChanges();
+    const result = component;
+    fixture.detectChanges();
 
-    it('should filter by limit date', () => {
+    expect(result).toBeTruthy();
+  });
 
-        component.tipIsInHour('2021-04-15T20:00:00');
-        fixture.detectChanges();
-        const result = component;
-        fixture.detectChanges();
+  it('should order by value', () => {
+    component.order_by_changed(2);
+    fixture.detectChanges();
+    const result = component;
+    fixture.detectChanges();
 
-        expect(result).toBeTruthy();
-    });
+    expect(result).toBeTruthy();
+  });
 
-    it('should order by value', () => {
+  it('should filter by min price', () => {
+    component.change_filter_min_price(2);
+    fixture.detectChanges();
+    const result = component;
+    fixture.detectChanges();
 
-        component.order_by_changed(2);
-        fixture.detectChanges();
-        const result = component;
-        fixture.detectChanges();
+    expect(result).toBeTruthy();
+  });
 
-        expect(result).toBeTruthy();
-    });
+  it('should filter by max price', () => {
+    component.change_filter_max_price(2);
+    fixture.detectChanges();
+    const result = component;
+    fixture.detectChanges();
 
-    it('should filter by min price', () => {
-
-        component.change_filter_min_price(2);
-        fixture.detectChanges();
-        const result = component;
-        fixture.detectChanges();
-
-        expect(result).toBeTruthy();
-    });
-
-    it('should filter by max price', () => {
-
-        component.change_filter_max_price(2);
-        fixture.detectChanges();
-        const result = component;
-        fixture.detectChanges();
-
-        expect(result).toBeTruthy();
-    });
-    
+    expect(result).toBeTruthy();
+  });
 });
