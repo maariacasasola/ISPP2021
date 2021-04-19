582bb0b6d14b4a63c8cfa23fdb548bba9c6c861b
Moisés Calzado <moisescalzado1227@gmail.com>
2021-04-19 12:36:28 +0200

    Fix tests

--- frontend/apps/gotacar/src/app/components/complaint-appeal-dialog/complaint-appeal-dialog.component.spec.ts
+++ frontend/apps/gotacar/src/app/components/complaint-appeal-dialog/complaint-appeal-dialog.component.spec.ts
@@ -2,7 +2,12 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {
+  MatDialogModule,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { RouterTestingModule } from '@angular/router/testing';
 import { of } from 'rxjs';
 import { ComplaintAppealsService } from '../../services/complaint-appeals.service';
@@ -11,42 +16,49 @@ import { ComplaintAppealDialogComponent } from './complaint-appeal-dialog.compon
 describe('ComplaintAppealDialogComponent', () => {
   let component: ComplaintAppealDialogComponent;
   let fixture: ComponentFixture<ComplaintAppealDialogComponent>;
-  let appealsService: ComplaintAppealsService;
 
   class mockComplaintsService {
     public create_complaint_appeal_complaint() {
       return of({
-        driver: {
-        },
-        complaint: {
-        },
-        content: "El retraso fue causado por necesidades personales, suelo ser puntual",
-        checked: false
-      })
+        driver: {},
+        complaint: {},
+        content:
+          'El retraso fue causado por necesidades personales, suelo ser puntual',
+        checked: false,
+      });
     }
 
     public create_complaint_appeal_banned() {
       return of({
-        driver: {
-        },
-        complaint: {
-        },
-        content: "El retraso fue causado por necesidades personales, suelo ser puntual",
-        checked: false
-      })
+        driver: {},
+        complaint: {},
+        content:
+          'El retraso fue causado por necesidades personales, suelo ser puntual',
+        checked: false,
+      });
     }
   }
 
   const mockDialogRef = {
-    close: jasmine.createSpy('close')
+    close: jasmine.createSpy('close'),
   };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ComplaintAppealDialogComponent],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-      imports: [RouterTestingModule, MatDialogModule, ReactiveFormsModule, HttpClientTestingModule],
-      providers: [{ provide: MAT_DIALOG_DATA, useValue: '' }, { provide: MatDialogRef, useValue: mockDialogRef }, { provide: ComplaintAppealsService, useClass: mockComplaintsService }],
+      imports: [
+        RouterTestingModule,
+        MatDialogModule,
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        MatSnackBarModule,
+      ],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: '' },
+        { provide: MatDialogRef, useValue: mockDialogRef },
+        { provide: ComplaintAppealsService, useClass: mockComplaintsService },
+      ],
     }).compileComponents();
   });
 
@@ -64,4 +76,4 @@ describe('ComplaintAppealDialogComponent', () => {
     component.close();
     expect(mockDialogRef.close).toHaveBeenCalled();
   });
-})
+});
