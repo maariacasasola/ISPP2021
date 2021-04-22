1af952b1c429d891c41d37c3ee0445590fe37706
Miguel Angel <mgbastechmology@gmail.com>
2021-04-22 19:24:30 +0200

    rating dialog ready

--- frontend/apps/gotacar/src/app/components/rating-user-dialog/rating-user-dialog.component.spec.ts
+++ frontend/apps/gotacar/src/app/components/rating-user-dialog/rating-user-dialog.component.spec.ts
@@ -11,6 +11,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 describe('RatingUserDialogComponent', () => {
   let component: RatingUserDialogComponent;
   let fixture: ComponentFixture<RatingUserDialogComponent>;
+  const mockDialogRef = {
+    close: jasmine.createSpy('close')
+  };
 
 
   beforeEach(async () => {
@@ -21,7 +24,7 @@ describe('RatingUserDialogComponent', () => {
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
       providers: [
     { provide: MAT_DIALOG_DATA, useValue: {} },
-    { provide: MatDialogRef, useValue: {} }
+    { provide: MatDialogRef, useValue: mockDialogRef }
   ]
     })
     .compileComponents();
@@ -36,4 +39,58 @@ describe('RatingUserDialogComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('#close() should close dialog', () => {
+    component.close();
+    expect(mockDialogRef.close).toHaveBeenCalled();
+  });
+  it('should mark form as touched', async () => {
+    component.rating_form.setValue({
+      content: '',
+    });
+    fixture.detectChanges();
+    const spy = spyOn(component.rating_form, 'markAllAsTouched');
+    expect(component.rating_form.valid).toBeFalsy();
+    component.onSubmit();
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+
+  it('should open snackbar when rating no provided', async () => {
+    component.rating_form.setValue({
+      content: 'Viaje agradable',
+    });
+    let rating;
+    fixture.detectChanges();
+    const spy = spyOn(component, 'openSnackBar');
+    expect(component.rating_form.valid).toBeTruthy();
+    component.onSubmit();
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      expect(spy).toHaveBeenCalled();
+    });
+    component.close();
+
+    expect(mockDialogRef.close).toHaveBeenCalled();
+  });
+  it('should open snackbar', () => {
+    spyOn(component, 'openSnackBar');
+    component.openSnackBar('hola');
+    fixture.detectChanges();
+    expect(component.openSnackBar).toHaveBeenCalled();
+  });
+  it('Should set rating',()=>{
+    let rating = "2"
+    component.rating=rating
+    spyOn(component,'set_rating');
+    component.set_rating(rating);
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(component.rating).toEqual(rating);
+    });
+
+    
+
+  })
 });
--- frontend/apps/gotacar/src/app/pages/authenticated-page/driver-trip-list-page/driver-trip-list-page.component.spec.ts
+++ frontend/apps/gotacar/src/app/pages/authenticated-page/driver-trip-list-page/driver-trip-list-page.component.spec.ts
@@ -1,5 +1,5 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ConvertCentToEurPipe } from '../../../pipes/convert-cent-to-eur.pipe';
 import { CommonModule, CurrencyPipe } from '@angular/common';
@@ -9,21 +9,30 @@ import { Observable, of } from 'rxjs';
 import { Trip } from '../../../shared/services/trip';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { User } from '../../../shared/services/user';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { DriverTripListPageComponent } from './driver-trip-list-page.component';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from 'apps/gotacar/src/environments/environment';
 import * as moment from 'moment';
+import { Router } from '@angular/router';
 
-class mockTripService{
-    get_driver_trips(){
-        return [];
-    }
+class mockTripService {
+  get_driver_trips() {
+    return [];
+  }
+  load_trips_by_driver(){
+    return [];
+  }
 }
 describe('DriverTripListPageComponent', () => {
   let component: DriverTripListPageComponent;
   let fixture: ComponentFixture<DriverTripListPageComponent>;
-
+  let router = {
+    navigate: jasmine.createSpy('navigate'),
+  };
+  const mockDialogRef = {
+    close: jasmine.createSpy('close'),
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -32,11 +41,13 @@ describe('DriverTripListPageComponent', () => {
         HttpClientTestingModule,
         MatDialogModule,
         MatSnackBarModule,
-        AngularFireModule.initializeApp(environment.firebaseConfig)
+        AngularFireModule.initializeApp(environment.firebaseConfig),
       ],
       declarations: [DriverTripListPageComponent, ConvertCentToEurPipe],
       providers: [
         { provide: TripsService, useClass: mockTripService },
+        { provide: Router, useValue: router },
+        { provide: MatDialogRef, useValue: mockDialogRef },
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
@@ -52,9 +63,19 @@ describe('DriverTripListPageComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('Can cancel', ()=>{
-    const startDate = moment().add(3,'hours');
+  it('Can cancel', () => {
+    const startDate = moment().add(3, 'hours');
     expect(component.can_cancel(startDate.toString())).toBeTruthy();
-  })
-  
+  });
+  it('navigate', fakeAsync(() => {
+    component.go_to_trip(1);
+    expect (router.navigate).toHaveBeenCalledWith(["/",
+       "authenticated",
+       "driver-trips",
+       1,]);
+  }));
+  it('Cancel date error', ()=>{
+    component.cancel(1,'2021/04/11');
+    expect(mockDialogRef.close).toHaveBeenCalled();
+  });
 });
