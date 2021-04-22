8500156c63e68470c747a5eb2b3c75d6c0b630e6
María <maariacasasola@gmail.com>
2021-04-22 14:21:36 +0200

    Ultimas modificaciones desde back

--- backend/src/main/java/com/gotacar/backend/controllers/ComplaintController.java
+++ backend/src/main/java/com/gotacar/backend/controllers/ComplaintController.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
