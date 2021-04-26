c134bba1d7759b8b4eb253833a6c115dac1ad975
Moisés Calzado <moisescalzado1227@gmail.com>
2021-04-26 13:33:59 +0200

    Fix del scroll a la hora de navegar entre páginas

--- frontend/apps/gotacar/src/app/app-routing.module.ts
+++ frontend/apps/gotacar/src/app/app-routing.module.ts
@@ -202,7 +202,9 @@ const routes: Routes = [
 
 // configures NgModule imports and exports
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'top'
+  })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
