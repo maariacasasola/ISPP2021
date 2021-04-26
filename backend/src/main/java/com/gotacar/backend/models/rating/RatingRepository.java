95f2ba29b391eb54dad54935265f4174e8a6e850
María <maariacasasola@gmail.com>
2021-04-26 16:19:27 +0200

    Elimino clases inutilizadas

--- backend/src/main/java/com/gotacar/backend/models/rating/RatingRepository.java
+++ backend/src/main/java/com/gotacar/backend/models/rating/RatingRepository.java
@@ -7,7 +7,7 @@ import com.gotacar.backend.models.User;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface RatingRepository extends MongoRepository<Rating, String>,RatingRepositoryCustom {
+public interface RatingRepository extends MongoRepository<Rating, String> {
     public Rating findById(ObjectId id);
 
     public List<Rating> findByTo(User to);
