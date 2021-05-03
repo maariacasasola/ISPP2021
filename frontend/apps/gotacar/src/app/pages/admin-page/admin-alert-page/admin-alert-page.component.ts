61c7c00e0e4590ebeca089a328f3935358a6b198
Moisés Calzado <moisescalzado1227@gmail.com>
2021-05-03 14:59:35 +0200

    Fixes

--- frontend/apps/gotacar/src/app/pages/admin-page/admin-alert-page/admin-alert-page.component.ts
+++ frontend/apps/gotacar/src/app/pages/admin-page/admin-alert-page/admin-alert-page.component.ts
@@ -20,33 +20,34 @@ export class AdminAlertPageComponent implements OnInit {
     Validators.required,
     Validators.maxLength(256),
   ]);
-  honeypot: FormControl = new FormControl(''); // we will use this to prevent spam
-  submitted: boolean = false; // show and hide the success message
-  isLoading: boolean = false; // disable the submit button if we're loading
-  responseMessage: string; // the response message to show to the user
-  
-  constructor(private formBuilder: FormBuilder, private http: HttpClient, private route:ActivatedRoute) {
+  honeypot: FormControl = new FormControl('');
+  submitted: boolean = false;
+  isLoading: boolean = false;
+  responseMessage: string;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private route: ActivatedRoute
+  ) {
     this.form = this.formBuilder.group({
       message: this.message,
       email: this.email,
       honeypot: this.honeypot,
     });
-    
   }
   ngOnInit(): void {
     this.email = this.route.snapshot.paramMap.get('user_email');
   }
 
   onSubmit() {
-    console.log("email: " + this.email)
     if (this.form.status == 'VALID' && this.honeypot.value == '') {
-      this.form.disable(); // disable the form if it's valid to disable multiple submissions
+      this.form.disable();
       var formData: any = new FormData();
       formData.append('email', this.email);
       formData.append('message', this.form.get('message').value);
-      this.isLoading = true; // sending the post request async so it's in progress
-      this.submitted = false; // hide the response message on multiple submits
+      this.isLoading = true;
+      this.submitted = false;
       this.http
         .post(
           'https://script.google.com/macros/s/AKfycbz7EwoK-15OdeQrSrMtDTkPKixhpkHtlon5Yhxle08GcUm41v16/exec',
@@ -54,7 +55,6 @@ export class AdminAlertPageComponent implements OnInit {
         )
         .subscribe(
           (response) => {
-            // choose the response message
             if (response['result'] == 'success') {
               this.responseMessage =
                 'El mensaje de alerta ha sido enviado satisfactoriamente.';
@@ -62,16 +62,16 @@ export class AdminAlertPageComponent implements OnInit {
               this.responseMessage =
                 'Oops! Algo ha ido mal... ¿Podrías refrescar la página o voler a intentarlo más tarde?.';
             }
-            this.form.enable(); // re enable the form after a success
-            this.submitted = true; // show the response message
-            this.isLoading = false; // re enable the submit button
+            this.form.enable();
+            this.submitted = true;
+            this.isLoading = false;
           },
           (error) => {
             this.responseMessage =
               'Oops! Ha habido un error... ¿Podrías refrescar la página o voler a intentarlo más tarde?.';
-            this.form.enable(); // re enable the form after a success
-            this.submitted = true; // show the response message
-            this.isLoading = false; // re enable the submit button
+            this.form.enable();
+            this.submitted = true;
+            this.isLoading = false;
           }
         );
     }
